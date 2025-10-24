a0f2f77e2811c6475832a85c9a0ee3d9243f2376
Lusine673 <snt-elechka@yandex.ru>
2025-10-24 18:59:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def meta_predict(df_norm):
     return logistic(np.dot(X, beta))
 
 # --- Заголовки ---
-st.markdown("<h3>Комплексная модель прогнозирования осложнений<br/>при наличии гестационного сахарного диабета</h3>", unsafe_allow_html=True)
+st.markdown("<h3>Комплексная модель прогнозирования акушерских и перинатальных осложнений<br/>при гестационном сахарноом диабете</h3>", unsafe_allow_html=True)
 
 # --- ВКЛАДКИ ---
 tab1, tab2, tab3, tab4 = st.tabs([
